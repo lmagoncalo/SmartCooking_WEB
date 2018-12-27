1e4e4028ad19b8fc1bd02e98f5e710b9eba4dc77
lmagoncalo <lmagoncalo@hotmail.com>
2018-12-27 21:22:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res) {
   res.sendfile("index.html");
 })
 
-app.get('/.*', function(req, res) {
+app.get('/*', function(req, res) {
   res.status(200);
   res.sendfile("index.html");
 })
