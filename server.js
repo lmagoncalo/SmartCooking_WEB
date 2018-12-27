361c20f422f7e7ecd12aa511838bea775b3ba31b
lmagoncalo <lmagoncalo@hotmail.com>
2018-12-27 18:27:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const port = process.env.PORT  || 8080
 express()
 .use(express.static(path.join(__dirname, 'public')))
 .get('/', (req, res) => res.render('index'))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+.listen(port, () => console.log(`Listening on ${ port }`))
